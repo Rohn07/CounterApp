b5c2e2b469db3d8fc2fbc31fa8299918569eef48
Rohn07 <Rohan.buddhdev08@gmail.com>
2020-09-21 20:19:33 +0530

    function done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counterApp';
+  count = 0;
+
+  increaseCount() {
+    this.count += 1;
+  }
+
+  decreaseCount() {
+    if (this.count != 0)
+    this.count -= 1;
+  }
+
+  resetCount()  {
+    this.count = 0;
+  }
 }
